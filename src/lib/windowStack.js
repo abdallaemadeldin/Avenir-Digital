ab665043359d466a0e8f847b5bbd2beec955757b
Abdalla Emad <abdalla.emad.eldin@gmail.com>
2021-08-15 19:05:00 +0200

    add home screen to stack

--- src/lib/windowStack.js
+++ src/lib/windowStack.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Splash from 'src/screens/Splash/Splash';
+import Home from 'src/screens/Home/Home';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,9 +11,8 @@ export default WindowStack = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Splash">
-                <Stack.Screen name="Splash" component={Splash} options={{
-                    headerShown: false
-                }} />
+                <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
+                <Stack.Screen name="Home" component={Home} />
             </Stack.Navigator>
         </NavigationContainer>
     );

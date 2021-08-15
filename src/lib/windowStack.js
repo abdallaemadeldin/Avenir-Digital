0c9d49b6467ac68f78c44f0c88ff2aeb30017e96
Abdalla Emad <abdalla.emad.eldin@gmail.com>
2021-08-15 21:43:33 +0200

    add screen to stack

--- src/lib/windowStack.js
+++ src/lib/windowStack.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Splash from 'src/screens/Splash/Splash';
 import Home from 'src/screens/Home/Home';
+import Profile from 'src/screens/Profile/Profile';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +12,15 @@ export default WindowStack = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Splash">
-                <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
-                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false, animation: "slide_from_bottom" }} />
+                <Stack.Screen name="Home" component={Home} options={{ animation: "slide_from_bottom" }} />
+                <Stack.Screen name="Profile" component={Profile} options={{
+                    animation: "slide_from_right",
+                    headerStyle: { backgroundColor: '#f1f1f1' },
+                    headerTintColor: "#000",
+                    title: '',
+                    headerBackTitle: "back"
+                }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
